14bf448c2a4ccbd332f83ef9c3eb9a6fcf26a3cf
kqlnei <kouami0907@icloud.com>
2023-11-07 04:53:59 +0900

    LFS

--- src/Assets/Scripts/Saeki_Scripts/BehindArea.cs
+++ src/Assets/Scripts/Saeki_Scripts/BehindArea.cs
@@ -1,26 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class BehindArea : MonoBehaviour
 {
+    public GameObject textObject;
+    public Image image;
+    private bool playerInsideArea = false;
+
     public bool behindEnter;
     // Start is called before the first frame update
     void Start()
     {
+        textObject.SetActive(false);
         behindEnter = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // ZÉLÅ[Ç™âüÇ≥ÇÍÇΩÇ∆Ç´
+        if (Input.GetKeyDown(KeyCode.Z) && playerInsideArea)
+        {
+            behindEnter = true;
+            // ImageUIÇï\é¶Ç∑ÇÈ
+            image.gameObject.SetActive(true);
+        }
     }
-    void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            behindEnter = true; Debug.Log(behindEnter);
+           // behindEnter = true; Debug.Log(behindEnter);
+            playerInsideArea = true;
+            textObject.SetActive(false);
         }
     }
 
@@ -28,6 +43,10 @@ public class BehindArea : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            Debug.Log("1");
+            image.gameObject.SetActive(false);
+            playerInsideArea = false;
+            textObject.SetActive(false);
             behindEnter = false;
         }
     }
