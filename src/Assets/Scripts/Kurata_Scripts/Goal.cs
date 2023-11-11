793a39b87bcc10502a7b57c95475544d609077c7
kqlnei <kouami0907@icloud.com>
2023-11-12 06:27:02 +0900

    Map,Goal
    壁、床、敵やった

---
+++ src/Assets/Scripts/Kurata_Scripts/Goal.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class Goal : MonoBehaviour
+{
+    public TMP_Text goalText;
+    // ColliderがGoalエリアに入ったらtrueにするフラグ
+    private bool goalEntered = false;
+    public bool goal = true;
+    // OnTriggerEnterはColliderが他のColliderに入った瞬間に呼ばれる
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // もしくは他のタグを使用
+        {
+            // プレイヤーがGoalエリアに入ったらフラグをtrueに設定
+            goalEntered = true;
+            // ここで結果シーンに遷移するメソッドを呼び出す
+            ShowGoalText();
+            Invoke("Result", 5f);
+            goal = false;
+        }
+    }
+
+    private void Result()
+    {
+        // フラグがtrueの場合、結果シーンに遷移
+        if (goalEntered)
+        {
+            SceneManager.LoadSceneAsync("ResultScene");
+        }
+    }
+
+    private void ShowGoalText()
+    {
+        // goalTextが存在し、まだ表示されていない場合
+        if (goalText != null && !goalText.gameObject.activeSelf)
+        {
+            goalText.gameObject.SetActive(true); // テキストを表示する
+        }
+    }
+}
