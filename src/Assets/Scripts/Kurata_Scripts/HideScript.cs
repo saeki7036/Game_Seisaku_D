c8a84e3bd0520bd943bf1b961c108dd58483e126
kqlnei <kouami0907@icloud.com>
2023-11-23 05:36:52 +0900

    ロッカーとライト変えた

--- src/Assets/Scripts/Kurata_Scripts/HideScript.cs
+++ src/Assets/Scripts/Kurata_Scripts/HideScript.cs
@@ -22,18 +22,21 @@ public class HideScript : MonoBehaviour
     private TextMeshProUGUI hidetextComponent;
 
     private CapsuleCollider capsuleCollider;
+
+    private bool isMoving = false; // 移動中のフラグ
     // Start is called before the first frame update
     void Start()
     {
         HidetextObject.SetActive(false);
         hidetextComponent = HidetextObject.GetComponent<TextMeshProUGUI>();
         capsuleCollider = GetComponent<CapsuleCollider>();
+        playerController = GetComponent<Playercontroller>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isPlayerInside)
+        if (isPlayerInside && !isMoving)
         {
             // プレイヤーがエリア内にいる場合の処理
             if (Gamepad.current.buttonSouth.wasReleasedThisFrame)// || Input.GetKeyDown(KeyCode.Z)
@@ -43,6 +46,8 @@ public class HideScript : MonoBehaviour
 
                 ChangeHide();
             }
+
+
 
         }
     }
@@ -61,7 +66,7 @@ public class HideScript : MonoBehaviour
             }
             capsuleCollider.enabled = false;
             hidetextComponent.text = "Press [A] to stop hiding";
-            MoveToTarget(nowPoint.position, InSidePoint.position);
+            StartCoroutine(MoveToTarget(nowPoint.position, InSidePoint.position));
 
         }
         else
@@ -75,20 +80,32 @@ public class HideScript : MonoBehaviour
                 boxCollider.isTrigger = false;
             }
             capsuleCollider.enabled = true;
-            nowPoint.LookAt(OutSidePoint);
             hidetextComponent.text = "Press [A] button";
-            MoveToTarget(nowPoint.position, OutSidePoint.position);
+            StartCoroutine(MoveToTarget(nowPoint.position, OutSidePoint.position));
+        
 
         }
     }
 
-    void MoveToTarget(Vector3 nowPosition, Vector3 targetPosition)
+    IEnumerator MoveToTarget(Vector3 nowPosition, Vector3 targetPosition)
     {
         // Keep the y-coordinate fixed
         targetPosition.y = 2f;
+        isMoving = true; // 移動中のフラグをセット
+        // 距離が一定以下になるまで補間を続ける
+        while (Vector3.Distance(transform.position, targetPosition) > 0.1f)
+        {
+            transform.LookAt(targetPosition);
+            transform.position = Vector3.Slerp(nowPosition, targetPosition, moveSpeed * Time.deltaTime);
+            nowPosition = transform.position;
 
-        // Move along the x and z axes using the targetPosition
-        transform.position = Vector3.Lerp(nowPosition, targetPosition, moveSpeed);
+            // 1フレーム待機
+            yield return null;
+        }
+
+        // 最終的な位置を目標の位置に設定（誤差をなくすため）
+        transform.position = targetPosition;
+        isMoving = false; // 移動が終わったらフラグをリセット
     }
 
     private void OnTriggerStay(Collider col)
@@ -97,9 +114,10 @@ public class HideScript : MonoBehaviour
         {
             Transform parentTransform = col.transform.parent;
             boxCollider = parentTransform.GetComponent<BoxCollider>();
-            playerController = GetComponent<Playercontroller>();
+
             isPlayerInside = true;
             HidetextObject.SetActive(true);
+            
             OutSidePoint = col.transform.GetChild(0);
             InSidePoint = col.transform.GetChild(1);
             nowPoint = this.transform;
