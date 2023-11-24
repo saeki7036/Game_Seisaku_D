0afff5ef499c3d15bafd8869be60e88b60223050
saeki7036 <114328780+saeki7036@users.noreply.github.com>
2023-11-24 21:49:36 +0900

    11/24 Enemy 変更

--- src/Assets/Scripts/Saeki_Scripts/VisibilityGizmo.cs
+++ src/Assets/Scripts/Saeki_Scripts/VisibilityGizmo.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class VisibilityGizmo : MonoBehaviour
 {
+    [SerializeField] private GameObject Enemy;
     private GameObject _Light_Gizmo;
+    private bool Mat_Change;
 
     [SerializeField, Range(0, 20)] private float _sight_range_Y = 5;
     [SerializeField, Range(0, 360)] private float _sight_angle_Y = 120;
     [SerializeField] private Material Mat_Y;
+    [SerializeField] private Material Mat_R;
+
+    [SerializeField] GameObject Visbility;
+    EnemyVisibility _VisbilityTrigger;
 
     // Start is called before the first frame update
     void Start()
     {
-        _Light_Gizmo = Instantiate(new GameObject(), Vector3.down * 3f + transform.position, Quaternion.Euler(Vector3.zero));
-        _Light_Gizmo.transform.parent = transform;
+        Mat_Change = false;
+
+        _Light_Gizmo = Instantiate(new GameObject(), Vector3.down * 3f + Enemy.transform.position, Enemy.transform.rotation);
+        _Light_Gizmo.transform.parent = Enemy.transform;
         _Light_Gizmo.AddComponent<MeshRenderer>();
         _Light_Gizmo.AddComponent<MeshFilter>();
         _Light_Gizmo.GetComponent<MeshRenderer>().material = Mat_Y;
 
-        List<Vector3> vertices = new() { Vector3.zero };
+        List <Vector3> vertices = new() { Vector3.zero };
         List<int> triangles = new();
        
         int i = 0;
@@ -43,11 +51,26 @@ public class VisibilityGizmo : MonoBehaviour
         mesh.RecalculateNormals();
 
         _Light_Gizmo.GetComponent<MeshFilter>().sharedMesh = mesh;
+
+        _VisbilityTrigger = Visbility.GetComponent<EnemyVisibility>();
+
+        _Light_Gizmo.transform.localPosition = Vector3.down;
     }
 
-// Update is called once per frame
-void Update()
+    // Update is called once per frame
+    void Update()
     {
-        
+        if (_VisbilityTrigger.visbilityEnter && !Mat_Change)
+        {
+            _Light_Gizmo.GetComponent<MeshRenderer>().material = Mat_R;
+            Mat_Change = true;
+        }
+            
+
+        else if(!_VisbilityTrigger.visbilityEnter && Mat_Change)
+        {
+            _Light_Gizmo.GetComponent<MeshRenderer>().material = Mat_Y;
+            Mat_Change = false;
+        }
     }
 }
