3ee3607ce5566fbaaf0991b0f1a98a1ac9eec136
kqlnei <kouami0907@icloud.com>
2023-11-30 03:53:43 +0900

    移動とダメージ
    移動アニメーション修正
    ダメージ処理変更
    移動に慣性をつけてゴーストっぽくした

--- src/Assets/Scripts/Saeki_Scripts/EnemyLight.cs
+++ src/Assets/Scripts/Saeki_Scripts/EnemyLight.cs
@@ -1,33 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyLight : MonoBehaviour
 {
     public bool lightEnter;
 
+    public Slider slider;
+
+    private float originalSliderValue;
+
+    private Coroutine returnTo100Coroutine;
     // Start is called before the first frame update
     void Start()
     {
         lightEnter = false;
+        slider.value = 100;
+        originalSliderValue = slider.value;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+              
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            lightEnter = true;
-            //Debug.Log("ダメージ");
-            LifeScript life = other.GetComponent<LifeScript>();
-            if (life != null)
+            
+            float decreaseSpeed = 5f;
+            float targetValue = slider.value - decreaseSpeed;
+
+            slider.value = Mathf.Lerp(slider.value, targetValue, Time.deltaTime * decreaseSpeed);
+
+            originalSliderValue = slider.value;
+
+            // プレイヤーがダメージを受けたらコルーチンを停止
+            StopCoroutine(returnTo100Coroutine);
+
+            if (slider.value < 1)
             {
-                life.TakeDamage(1);
+                lightEnter = true;
+                LifeScript life = other.GetComponent<LifeScript>();
+                if (life != null)
+                {
+                    life.TakeDamage(1);
+                }
+                slider.value = 100;
+               
             }
         }
     }
@@ -37,6 +60,23 @@ public class EnemyLight : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             lightEnter = false;
+            // コルーチンを開始し、そのCoroutineオブジェクトへの参照を保持
+            returnTo100Coroutine = StartCoroutine(ReturnSliderTo100OverTime());
         }
+    }
+
+    IEnumerator ReturnSliderTo100OverTime()
+    {
+        float elapsedTime = 0f;
+        float duration = 10f;
+
+        while (elapsedTime < duration)
+        {
+            slider.value = Mathf.Lerp(originalSliderValue, 100, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        slider.value = 100;
     }
 }
